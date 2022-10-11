916d950b0a4db655e207d1e860c5a960f74a22f5
Franco Quevedo <franco.03_93@live.com.ar>
2022-10-11 12:14:50 -0300

    clase11-Arrays

---
+++ clase11-Condicionales-arrays/js/main.js
@@ -0,0 +1,46 @@
+//ACTIVIDAD DE CLASE//
+//Actividad 1: Crea un array de strings con 5 elementos que quieras. Luego, mostrá en consola:
+// 1 El array
+//2 Cuántos elementos tiene el array
+// 3 El índice 0 del array*/
+
+/* let misMarcas= ["nike","adidas","puma"];
+[" lenovo","samsung","hp"];
+[" philips","tophouse"]
+;
+
+console.log(misMarcas);
+console.log(misMarcas.lenght);
+console.log(misMarcas[0]);
+ */
+
+
+//* switch //
+
+let numeroDia= new Date.().get Day(); 
+
+switch (numeroDia){
+case 0:
+    alert("domingo");
+    break;
+    case 1:
+    alert("lunes");
+    break;
+    case 2:
+    alert("martes");
+    break;
+    case 3:
+    alert("miercoles");
+    break;
+    case 4:
+    alert("jueves");
+    break;
+    case 5:
+    alert("viernes");
+    break;
+    case 6:
+        alert("sabado");
+        break;
+   
+
+}
