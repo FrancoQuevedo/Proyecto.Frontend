3f95d95c0b2fbf5f426f8a5c7056245bad572245
Franco Quevedo <franco.03_93@live.com.ar>
2022-11-10 10:29:02 -0300

    C-20-react

---
+++ clase19-ReactJS-/my-react-app/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import prueba from './prueba.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       {/*  <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={prueba} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.Soy Franaco .
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+<div  className="contenedor"> <h3 className='texto'>Hola Mundo</h3></div>
+
+    </div>
+
+  );
+}
+
+export default App;
